82db9a564d7ffbd59390c3533254020be4242ac7
Александр Федоткин <ben115899@gmail.com>
2024-09-23 21:55:34 +0700

    Create and configure project

---
+++ EltexFirstProject/App/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  EltexFirstProject
+//
+//  Created by Александр Федоткин on 23.09.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
---
+++ EltexFirstProject/Modules/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EltexFirstProject
+//
+//  Created by Александр Федоткин on 23.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
