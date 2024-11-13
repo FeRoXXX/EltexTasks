479225d0a397a98019662498ab0ed6a05bdd3917
Александр Федоткин <ben115899@gmail.com>
2024-11-13 11:56:15 +0700

    Add new task

---
+++ EltexNews/EltexNews/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  EltexNews
+//
+//  Created by Александр Федоткин on 13.11.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.makeKeyAndVisible()
+        window.rootViewController = UINavigationController(rootViewController: NewsListViewController())
+        self.window = window
+    }
+
+}
+
---
+++ EltexNews/EltexNews/Modules/NewsListModule/View/NewsListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewsListViewController.swift
+//  EltexNews
+//
+//  Created by Александр Федоткин on 13.11.2024.
+//
+
+import UIKit
+
+final class NewsListViewController: UIViewController {
+    
+    //MARK: - Lifecycle functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
